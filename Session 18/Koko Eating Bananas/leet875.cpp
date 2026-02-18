461ec1f8ae48a2a7b03b47bb1de3a87a20e8b3f8
Disha <24BCS10773@cuchd.in>
2026-02-18 22:43:01 +0530

    Leetcode 875

--- Session 18/Koko Eating Bananas/leet875.cpp
+++ Session 18/Koko Eating Bananas/leet875.cpp
@@ -1,1 +1,28 @@
-
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int n = piles.size();
+        int max_piles = 0;
+        for (int i = 0; i < n; i++) {
+            if (piles[i] > max_piles)
+                max_piles = piles[i];
+        }
+        int low = 1;
+        int high = max_piles;
+        int ans = max_piles;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            long long totalHours = 0;
+            for (int i = 0; i < n; i++) {
+                totalHours += (piles[i] + mid - 1) / mid;
+            }
+            if (totalHours <= h) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
