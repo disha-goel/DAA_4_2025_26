ef0d2bda549c270c25326c73906dcb4975aa6537
Disha <24BCS10773@cuchd.in>
2026-02-18 23:38:45 +0530

    Painter's Partition Problem

--- Session 18/GFG_Codes/Painter's_Partition.cpp
+++ Session 18/GFG_Codes/Painter's_Partition.cpp
@@ -1,1 +1,36 @@
+class Solution {
+  public:
+         bool canPaint(vector<int>& arr, int k, long long maxTime) {
+        int painters = 1;
+        long long curr = 0;
+        for (int i = 0; i < arr.size(); i++) {
+            if (curr + arr[i] > maxTime) {
+                painters++;
+                curr = arr[i];
+                if (painters > k) return false;
+            } else {
+                curr += arr[i];
+            }
+        }
+        return true;
+    }
 
+    int minTime(vector<int>& arr, int k) {
+        long long low = *max_element(arr.begin(), arr.end());
+        long long high = 0;
+        for (int x : arr) high += x;
+
+        long long ans = high;
+        while (low <= high) {
+            long long mid = low + (high - low) / 2;
+            if (canPaint(arr, k, mid)) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+        
+    }
+};
