35eed69a2f1c7e8f9b73620df641c7be99e681c8
Disha <24BCS10773@cuchd.in>
2026-03-10 16:00:32 +0530

    GFG K Sized Subarray Maximum
    Refactored maxOfSubarrays function to improve efficiency by implementing an optimal approach using a deque.

--- Lab_MST/subarray_max.cpp
+++ Lab_MST/subarray_max.cpp
@@ -1,9 +1,14 @@
+//UID:24BCS10773
+
+//GFG: K Sized Subarray Maximum
+
+//BRUTE FORCE
 class Solution {
   public:
     vector<int> maxOfSubarrays(vector<int>& arr, int k) {
         vector<int>res;
        int l=0;
-       int r=l+k;
+       int r=k;
        while(r<=arr.size()){
            int maxE=arr[l];
        for(int i=l;i<r;i++){
@@ -16,6 +21,25 @@ class Solution {
        r++;
        }
         return res;
-        
     }
 };
+
+//OPTIMAL APPROACH
+class Solution {
+  public:
+    vector<int> maxOfSubarrays(vector<int>& arr, int k) {
+  deque<int> dq;
+    vector<int> ans;
+    for(int i = 0; i < arr.size(); i++) {
+        if(!dq.empty() && dq.front() <= i - k)
+            dq.pop_front();
+        while(!dq.empty() && arr[dq.back()] < arr[i])
+            dq.pop_back();
+        dq.push_back(i);
+        if(i >= k - 1)
+            ans.push_back(arr[dq.front()]);
+    }
+    return ans;
+}};
+
+
